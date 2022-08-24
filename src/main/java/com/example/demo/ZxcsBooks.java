9c150c111c6f42d0dbad26cd273715a267081ea4
zzy <475698058@qq.com>
2022-08-24 09:26:28 +0800

    增加book评分爬取

---
+++ src/main/java/com/example/demo/ZxcsBooks.java
@@ -0,0 +1,52 @@
+package com.example.demo;
+
+import cn.hutool.core.convert.Convert;
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.http.HttpRequest;
+import cn.hutool.http.HttpResponse;
+import cn.hutool.http.HttpUtil;
+import cn.hutool.json.JSONUtil;
+import com.sun.net.httpserver.HttpServer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Zero
+ */
+public class ZxcsBooks {
+    public static void main(String[] args) {
+        String scoreUrl = "http://zxcs.me/content/plugins/cgz_xinqing/cgz_xinqing_action.php?action=show&id=";
+        String contentUrl = "http://zxcs.me/post/";
+        String downloadUrl = "http://zxcs.me/download.php?id=";
+
+        //max 12300
+        List<String> scoreList = new ArrayList<>();
+        for (int i = 0; i < 12300; i++) {
+            HttpResponse response = HttpRequest.get(scoreUrl + i).execute();
+            if (!response.isOk()) {
+                continue;
+            }
+
+            String text = response.body();
+            int good = Convert.toInt(text.split(",")[0], -1);
+            if (good < 1000) {
+                continue;
+            }
+            scoreList.add(good + "-" + i);
+        }
+        scoreList.sort((a, b) -> b.compareTo(a));
+
+        List<String> titleList = new ArrayList<>();
+        for (String item : scoreList) {
+            String[] str = item.split("-");
+            String id = str[1];
+            HttpResponse resp = HttpRequest.get(contentUrl + id).execute();
+            String text2 = resp.body();
+            String title = text2.substring(text2.indexOf("<title>《") + 7, text2.indexOf("》") + 1);
+            titleList.add(title + ",id:" + id + ",good：" + str[0]);
+        }
+
+        System.out.println(JSONUtil.toJsonStr(titleList));
+    }
+}
